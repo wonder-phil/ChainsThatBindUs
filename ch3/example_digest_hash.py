b9fd1bf3b06e59e1340c22530a3ee2cdb25e0430
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2025-03-06 13:51:14 -0500

    Update example_digest_hash.py

--- ch3/example_digest_hash.py
+++ ch3/example_digest_hash.py
@@ -21,12 +21,14 @@ class ExampleHash:
 # The next example shows aspects of avalanche criteria
 #
 
-e = ExampleHash();
-
-e.compHash("Hello");
-e.compHash("Hello1");
-e.compHash("Hello2");
-e.compHash("Hello2453453453453345353453454564564");
-
+Python3> e = ExampleHash()
+Python3> e.compHash("Hello")
+> 185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969
+Python3> e.compHash("Hello1")
+> 0d8741d6fca6218ca61c7180eeb4f97f57999e0611da71127222bc6942864dbae
+Python3> e.compHash("Hello2")
+> 7710852ea8b0ff3921f7086c9694483b214118e4308ff4b25ef114fdbf0fcd6f
+Python3> e.compHash("Hello2453453453453345353453454564564")
+> edf354bae21ef473702ba3af4c9090b4366907357bac21f6624ab880e01f6ce1
 
     
